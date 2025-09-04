80d0cd754394c67a137e1fd71e25b154a82dfd91
rymeb98 <rymeb98@gmail.com>
2025-09-04 15:44:14 -0400

    AVONET class work

---
+++ AVONET1_BirdLife.R
@@ -0,0 +1,21 @@
+read.csv("AVONET1_BirdLife.csv")
+"AVONET1_BirdLife.Rproj"
+read.csv("C:/Users/yairb/Documents/Statistical_Ecology/AVONET1.BirdLife.csv")
+
+read.csv("C:/Users/yairb/Documents/Statistical Ecology/AVONET1_BirdLife.csv")
+read.csv("/AVONET1_BirdLife.csv")
+getwd()
+read.csv("AVONET1_BirdLife.csv")
+class("something")
+class(1)
+class(1L)
+class(TRUE)
+class("AVONET1_BirdLife.csv")
+avonet=read.csv("AVONET1_BirdLife.csv")
+class(avonet)
+avonet<-read.csv("AVONET1_BirdLife.csv")
+avonet$Species1
+avonet[,2]
+avonet[2]
+class(avonet$Species1)
+str(avonet)
