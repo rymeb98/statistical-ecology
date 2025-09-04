6ce3d7e35f076c3f951df0958b60695cf349d2d8
rymeb98 <rymeb98@gmail.com>
2025-09-04 15:39:34 -0400

    1.7 classwork and Stat Eco Rproj

---
+++ 1.7 classwork.R
@@ -0,0 +1,37 @@
+5*19
+sqrt(81)
+x = 3*4/sqrt(25)
+x
+x*34
+q()
+help.start()
+x
+q()
+?sqrt
+?matrix
+install.packages(c("ellipse","plotrix"))
+install.packages(c("ellipse","plotrix"))
+help.start()
+set.seed(101)
+set.seed
+frogs = c(1.1,1.3,1.7,1.8,1.9,2.1,2.3,2.4,2.5,2.8,3.1,3.3,3.6,3.7,3.9,4.1,4.5,4.8,5.1,5.3)
+tadpoles = rnorm(n = 20, mean = 2*frogs, sd = 0.5)
+frogs
+tadpoles
+plot(frogs, tadpoles)
+abline(a = 0, b = 2)
+log_tadpoles = log(tadpoles)
+plot(frogs, log_tadpoles)
+plot(frogs,log(tadpoles))
+plot(frogs,tadpoles,log="y")
+log10(tadpoles)
+n = 1:length(frogs)
+plot(n, frogs)
+plot(frogs)
+mean(tadpoles)
+sd(tadpoles)
+summary(tadpoles)
+cor(frogs, tadpoles)
+cor.test(frogs, tadpoles)
+?cor.test
+help.search("correlation"")
